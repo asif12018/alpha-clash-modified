2140700450f8a82fd228ff4542959b60409cae65
asif12018 <asif.request@gmail.com>
2024-02-14 12:52:31 +0600

    almost complete

--- js/app.js
+++ js/app.js
@@ -1,7 +1,49 @@
 
 //starting home code:
 
+
+function continueGame(){
+    gameData = getARandomAlphabet();
+    document.getElementById('target').innerText = gameData;
+    playSound('game');
+    return gameData;
+}
+
+function startGame(){
+    let sum = 0;
+    let gameData = continueGame();
+    document.addEventListener('keyup',function(event){
+        console.log('this is game data', gameData);
+        console.log('this is key',event.key);
+        if(event.key === gameData){
+            document.getElementById(event.key).style.background = 'white';
+            playSound('user');
+            gameData = continueGame();
+            sum++;
+            document.getElementById('life-board').innerText = sum;
+            setTimeout(function(){
+                document.getElementById(event.key).style.background = '#191e24';
+            }, 100);
+        }else if(event.key !== gameData){
+            score();
+            playSound('wrong');
+            document.getElementById('result').innerText = sum;
+        }
+    })
+}
+
+function score(){
+    hideElementById('play-ground');
+    showElementById('score');
+    // document.getElementById('score-btn').addEventListener('click',function(){
+    //     play();
+    // })
+}
+
+
 function play(){
     hideElementById('home')
     showElementById('play-ground');
+    startGame();
+
 }
--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,28 @@
+
+//audio function
+
+
+
+
+function playSound(sound){
+    
+    
+    
+    if(sound === 'game'){
+        let gameSelect = new Audio('../audio/red.mp3');
+        gameSelect.play();
+    }else if(sound === 'user'){
+        let userSelect = new Audio('../audio/green.mp3');
+        userSelect.play();
+    }else{
+        let wrong = new Audio('../audio/wrong.mp3');
+        wrong.play();
+    }
+}
+
+
+
+//game data generator
 function hideElementById(element){
     document.getElementById(element).classList.add('hidden');
 }
@@ -5,3 +30,12 @@ function hideElementById(element){
 function showElementById(element){
     document.getElementById(element).classList.remove('hidden');
 }
+
+function getARandomAlphabet(){
+    const alphabetString = 'qwertyuiopasdfghjklzxcvbnm/';
+    const alphabetArray = alphabetString.split('');
+    
+    const num = Math.round(Math.random() * 26);
+    return alphabetArray[num];
+}
+
