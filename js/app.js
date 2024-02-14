993e4c3a6acf9c00c4d78d378140ee278cf7e129
asif12018 <asif.request@gmail.com>
2024-02-14 13:06:55 +0600

    audio issue fixed

--- js/app.js
+++ js/app.js
@@ -47,18 +47,3 @@ function play(){
 
 
 
-function playSound(sound){
-    
-    
-    
-    if(sound === 'game'){
-        let gameSelect = new Audio('../audio/red.mp3');
-        gameSelect.play();
-    }else if(sound === 'user'){
-        let userSelect = new Audio('../audio/green.mp3');
-        userSelect.play();
-    }else{
-        let wrong = new Audio('../audio/wrong.mp3');
-        wrong.play();
-    }
-}
